706427bc5eca0dfa82a011f64f99ff84a401b6ba
Viewless <nikola.dionisiev@gmail.com>
2018-08-27 17:32:31 +0300

    Second

---
+++ 01_Cortex/ChattingServer/ChattingService.cs
@@ -0,0 +1,16 @@
+﻿using ChattingInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChattingServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ChattingService" in both code and config file together.
+    public class ChattingService : IChattingService
+    {
+       
+    }
+}
